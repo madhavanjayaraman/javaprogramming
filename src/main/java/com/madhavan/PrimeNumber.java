e52d53002c365212fff5fb801168132795fc5e19
Madhavan Jayaraman <Madhavan.Jayaraman@fisglobal.com>
2020-03-08 16:23:06 +0530

    Initial Commit

---
+++ src/main/java/com/madhavan/PrimeNumber.java
@@ -0,0 +1,13 @@
+package com.madhavan;
+
+/**
+ * author e3016939, Madhavan Jayaraman
+ * date 3/8/2020 , time 4:18 PM
+ * project_name javaprogramming
+ * package_name com.madhavan\
+ **/
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println("Test Prime Number");
+    }
+}
