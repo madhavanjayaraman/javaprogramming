f2d5db3e524a8903072186bcc81c5b53c1dd7ae0
Madhavan Jayaraman <Madhavan.Jayaraman@fisglobal.com>
2021-06-03 16:42:50 +0530

    Maximum Occurence from the string

---
+++ src/main/java/com/madhavan/geeksforgeeks/MaximumOccurence.java
@@ -0,0 +1,47 @@
+package com.madhavan.geeksforgeeks;
+
+/**
+ * author e3016939, Madhavan Jayaraman
+ * date 6/3/2021 , time 4:03 PM
+ * project_name javaprogramming
+ * package_name com.madhavan.geeksforgeeks\
+ **/
+
+/**
+ * Return maximum occurring character in an input string
+ */
+public class MaximumOccurence {
+    /*
+    Write an efficient function to return maximum occurring character in the input string
+    e.g., if input string is “test” then function should return ‘t’.
+     */
+
+    static final int ASCII_SIZE = 256;
+
+    static char getMaxOccuring(String str){
+        char result = ' ';
+
+        int count[] = new int[ASCII_SIZE];
+
+        int len = str.length();
+
+        for(int i=0; i<len;i++) count[str.charAt(i)]++;
+
+        int max = -1;
+        for(int i=0;i<len;i++) {
+            if(max < count[str.charAt(i)]) {
+                max = count[str.charAt(i)];
+                result = str.charAt(i);
+            }
+        }
+
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        final String inputStr = "This is a random check am Madhavan";
+        System.out.println(getMaxOccuring(inputStr));
+    }
+}
